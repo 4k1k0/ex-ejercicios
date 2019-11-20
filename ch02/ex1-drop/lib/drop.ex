2425501cb185f855a473ba99553bd7d12072f920
Wako <dulo.rodrigo@gmail.com>
2019-11-19 19:52:06 -0600

    Docs

--- ch02/ex1-drop/lib/drop.ex
+++ ch02/ex1-drop/lib/drop.ex
@@ -6,6 +6,9 @@ defmodule Drop do
   @doc """
   Calculates the velocity of an object falling on Earth as if it were in a vacuum (no air resistance). The distance is the height from which the object falls, specified in meters, and the function returns a velocity in meters per second
   """
+  
+  @spec fall_velocity(number(), number()) :: float()
+  
   def fall_velocity(distance, gravity \\ 9.8) do
     :math.sqrt(2 * gravity * distance)
   end
