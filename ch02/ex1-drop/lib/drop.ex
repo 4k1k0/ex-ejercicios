ec57a19668bec5d30d713d6e148e039ed3d5dc2a
Wako <dulo.rodrigo@gmail.com>
2019-11-25 16:56:38 -0600

    Guardia y funcp

--- ch02/ex1-drop/lib/drop.ex
+++ ch02/ex1-drop/lib/drop.ex
@@ -7,17 +7,21 @@ defmodule Drop do
   Calculates the velocity of an object falling on Earth as if it were in a vacuum (no air resistance). The distance is the height from which the object falls, specified in meters, and the function returns a velocity in meters per second
   """
   
-  @spec fall_velocity(number(), number()) :: float()
+  #@spec fall_velocity(number(), number()) :: float()
   
-  def fall_velocity(:earth, distance) do
+  def fall_velocity({planemo, distance}) when distance >= 0 do
+    fall_velocity(planemo, distance)
+  end
+  
+  defp fall_velocity(:earth, distance) do
     :math.sqrt(2 * 9.8 * distance)
   end
 
-  def fall_velocity(:moon, distance) do
+  defp fall_velocity(:moon, distance) do
     :math.sqrt(2 * 1.6 * distance)
   end
 
-  def fall_velocity(:mars, distance) do
+  defp fall_velocity(:mars, distance) do
     :math.sqrt(2 * 3.71 * distance)
   end
 
