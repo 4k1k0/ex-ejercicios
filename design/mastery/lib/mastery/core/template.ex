86d2ac2eb556141816d39b091d4905fb48fdc43f
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2025-12-16 22:29:05 -0600

    build functional core quiz

---
+++ design/mastery/lib/mastery/core/template.ex
@@ -0,0 +1,13 @@
+defmodule Mastery.Core.Template do
+  defstruct ~w[name category instructions raw compiled generators checker]a
+
+  @spec new(Keyword.t()) :: struct()
+  def new(fields) do
+    raw = Keyword.fetch!(fields, :raw)
+
+    struct!(
+      __MODULE__,
+      Keyword.put(fields, :compiled, EEx.compile_string(raw))
+    )
+  end
+end
